5d97e8545cbf3434082300f18d3b8b1b87fb56cd
eggann <59432847+eggann@users.noreply.github.com>
2022-03-06 05:50:46 +0800

    Update api.py

--- view/api.py
+++ view/api.py
@@ -11,7 +11,7 @@ def db_connection():
         port = 3306,
         user = "root",
         database = "travel",
-        password = "",
+        password = "jiggjo9182",
         charset = "utf8"
         )
     except mysql.connector.Error as e:
