b4ce53d9c55238201aa25f15b174fdac8fb73c80
eggann <59432847+eggann@users.noreply.github.com>
2022-03-13 18:32:34 +0800

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,69 +1,152 @@
-function main(){
-    let src = "http://192.168.18.3:3000/api/attractions";
-    fetch(src)
-    .then((response) => {
-        return response.json();
-    }).then((result) => {
-        let content = document.createElement('div');
-        document.body.appendChild(content);
-        content.setAttribute("class", "content");
+let insert = document.getElementsByClassName("insert").value;
+let page = 0;
+let isFetchingMore = false;
+let noData = false;
 
-        Array.from(Array(12)).forEach((_, index) => {
-            /**
-             * all section
-             */
-            const all = document.createElement('div');
-            content.appendChild(all);
-            all.setAttribute("class", "all");
-        
-            /**
-             * img section
-             */
-            const myString = result.data[index]["images"][0];
-            const img = document.createElement('img');
-            all.appendChild(img);
-            img.setAttribute("src", myString);
-            img.setAttribute("class", "img");
-        
-            /**
-             * name section
-             */
-            const title = document.createElement('div');
-            all.appendChild(title);
-            title.setAttribute("class", "title");
+function keyword(){
+  const main = document.querySelector("main");
+  let input = document.getElementById("ins").value;
+  main.innerHTML = '';
+  let divOuter= document.createElement("div");
+  divOuter.id="siteOuter";
+  divOuter.className="siteOuter";
+  main.appendChild(divOuter);
+  let currentPage=0;
+  let src = `http://localhost:3000/api/attractions?page=${currentPage}&keyword=${insert}`;
+  fetch(src).then(res => {return res.json();
+  }).then(result => {
+    if(result.data != null) {
+      main.innerHTML = updateHtml(result);
+    }
+    else {
+      main.innerHTML = `找不到${insert}的相關景點唷`;
+    }
+  })
+}
 
-            const nameString = result.data[index]["name"];
-            const name = document.createElement('div');
-            title.appendChild(name);
-            name.setAttribute("class", "name");
-            name.textContent = nameString;
+// function keyword() {
+//   let main = document.querySelector("main");
+//   main.innerHTML = '';
+//   let divOuter= document.createElement("div");
+//   const src = `http://localhost:3000/api/attractions?page=${page}&keyword=${insert}`;
+//   fetch(src).then((res) => {
+//     return res.json();
+//   }).then(result => {
+//     if(result.error){
+//       divOuter.innerHTML = `找不到${insert}的相關景點唷`;
+//       // console.log(insert);
+//     }
+//     else {
+//       updateHtml(result);
+//       // console.log(insert);
+//     }
+//   })
+// };
 
-            const categoryString = result.data[index]["category"];
-            const category = document.createElement('div');
-            title.appendChild(category);
-            category.setAttribute("class", "category");
-            category.textContent = categoryString;
+async function attractions() {
+  if (isFetchingMore || noData) return null;
 
-            const metString = result.data[index]["mrt"];
-            const mrt = document.createElement('div');
-            title.appendChild(mrt);
-            mrt.setAttribute("class", "mrt");
-            mrt.textContent = metString;
-            // name.textContent = result.result.results[index].stitle;
-            // name.setAttribute("class", "img_name");
+  console.log("fetching!");
+  isFetchingMore = true;
 
-            // console.log(result.data[index])
-        });
-        let footer = document.createElement('footer');
-        footer.textContent = 'COPYRIGHT © 2021 台北一日遊';
-        document.body.appendChild(footer);
+  const src = `http://localhost:3000/api/attractions?page=${page}`;
 
-        console.log(result.nexPage);
+  const response = await fetch(src).then((res) => {
+    page += 1;
+    isFetchingMore = false;
+    return res.json();
+  });
 
-        window.addEventListener('scroll', ()=>{
-            // console.log(result.data);
-        });
-    });
-};
+  if (response.data.length === 0) {
+    noData = true;
+  }
+
+  return response;
+}
+
+function updateHtml(response) {
+  if (response === null || noData) return;
+
+  const main = document.querySelector("main");
+  let content = document.querySelector(".content");
+
+  if (!content) {
+    content = document.createElement("div");
+    content.setAttribute("class", "content");
+    main.appendChild(content);
+  }
+
+  Array.from(response.data).forEach((_, index) => {
+    /**
+     * all section
+     */
+    const all = document.createElement("div");
+    content.appendChild(all);
+    all.setAttribute("class", "all");
+
+    /**
+     * img section
+     */
+    const myString = response.data[index]["images"][0];
+    const img = document.createElement("img");
+    all.appendChild(img);
+    img.setAttribute("src", myString);
+    img.setAttribute("class", "img");
+
+    /**
+     * name section
+     */
+    const title = document.createElement("div");
+    all.appendChild(title);
+    title.setAttribute("class", "title");
+
+    const nameString = response.data[index]["name"];
+    const name = document.createElement("div");
+    title.appendChild(name);
+    name.setAttribute("class", "name");
+    name.textContent = nameString;
+
+    const categoryString = response.data[index]["category"];
+    const category = document.createElement("div");
+    title.appendChild(category);
+    category.setAttribute("class", "category");
+    category.textContent = categoryString;
+
+    const metString = response.data[index]["mrt"];
+    const mrt = document.createElement("div");
+    title.appendChild(mrt);
+    mrt.setAttribute("class", "mrt");
+    mrt.textContent = metString;
+  });
+}
+
+/**　https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API */
+function createObserver() {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(async (entry) => {
+        if (entry.isIntersecting) {
+          // 拉新的資料
+          const res = await attractions();
+          updateHtml(res);
+          console.log("Done!");
+        }
+      });
+    },
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    }
+  );
+
+  return observer;
+}
+
+async function main() {
+  const observer = createObserver();
+
+  observer.observe(document.querySelector("footer"));
+}
 
 main();
